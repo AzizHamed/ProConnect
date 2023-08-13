b54bf541b9741338545e148bed528ea3423ab711
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-13 16:34:33 +0300

    Created empty projects

---
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/ProConnectApplication.java
@@ -0,0 +1,14 @@
+package com.braude.ProConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProConnectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		SpringApplication.run(ProConnectApplication.class, args);
+	}
+
+}
