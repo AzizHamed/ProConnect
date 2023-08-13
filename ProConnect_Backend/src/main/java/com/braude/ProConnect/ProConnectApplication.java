b95d37e14aef9b2c4e9ee3d511a81d80f1412604
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-13 18:36:05 +0300

    Update ProConnectApplication.java

--- ProConnect_Backend/src/main/java/com/braude/ProConnect/ProConnectApplication.java
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/ProConnectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProConnectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
 		SpringApplication.run(ProConnectApplication.class, args);
 	}
 
