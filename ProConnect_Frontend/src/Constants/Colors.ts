730ef13d3c550abb9c80af1a62056a7021d45968
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-21 22:46:58 +0300

    Added documentation to Requests and Colors

--- ProConnect_Frontend/src/Constants/Colors.ts
+++ ProConnect_Frontend/src/Constants/Colors.ts
@@ -1,9 +1,15 @@
-// Colors.ts
+/**
+ * This file contains predefined color schemes that can be used in the application.
+ */
+
+/**
+ * This interface defines the primary colors used in the various parts of the application.
+ */
 interface ColorScheme {
     background: string;
     container: string;
     highlight: string;
-    black: string;
+    textColor: string;
     star: string;
   }
   
@@ -11,20 +17,23 @@ interface ColorScheme {
     background: "#CFE8FB",
     container: "#FFFFFF7D",
     highlight: "#0F30F7",
-    black: "#000000",
+    textColor: "#000000",
     star: "#F2AF0D",
   };
   
   const Dark: ColorScheme = {
-    background: "#000000",
-    container: "#FFFFFF7D",
-    highlight: "#0F30F7",
-    black: "#000000",
+    background: "#222222",
+    container: "#5353537d",
+    highlight: "#7415c2",
+    textColor: "#FFFFFF",
     star: "#F2AF0D",
   };
   
   const Themes: Record<string, ColorScheme> = { Light, Dark };
   
+  /**
+   * @returns ColorScheme based on the desired theme.
+   */
   function getColors(theme: string = 'Light'): ColorScheme {
     return Themes[theme] || Light; // Default to Light theme if the specified theme doesn't exist
   }
--- ProConnect_Frontend/src/Services/Requests.ts
+++ ProConnect_Frontend/src/Services/Requests.ts
@@ -2,17 +2,30 @@ import axios, { AxiosResponse } from "axios";
 
 const baseUrl = 'http://localhost:8080';
 
-// Passing configuration object to axios
+/**
+ * Get data from the server.
+ * @param url URL mapping for the required API endpoint. For example, users/getUser
+ * @param paramsMap Map containing optional parameters that get sent in the URL, where the keys represent the parameter names, and the values are the parameter values.
+ * For example: @example users/getUser?userId=14&name=Hadi
+ * @returns A promise containing the result of the request.
+ */
 function get(url:string, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>{
     let response = axios({
       method: 'get',
       url: `${baseUrl}/${url}`,
       params: paramsMapToObject(paramsMap),
-      data: {body:"HELLO! THIS IS A DATA BLOCK!"}
     });
     return response;
 }
 
+/**
+ * Post data to the server.
+ * @param url URL mapping for the required API endpoint. For example, users/updateUser
+ * @param data JSON data that gets sent as the request body.
+ * @param paramsMap Map containing optional parameters that get sent in the URL, where the keys represent the parameter names, and the values are the parameter values.
+ * For example: @example users/updateUser?userId=14
+ * @returns A promise containing the result of the request.
+ */
 function post(url:string, data?: any, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>{
     let response = axios({
       method: 'post',
@@ -24,6 +37,10 @@ function post(url:string, data?: any, paramsMap?: Map<string, any>): Promise<Axi
     return response;
 }
 
+/**
+ * Converts a parameter map into an object that can be used as request parameters. @example
+ * A map containing [<"name", "Hadi">, <"email", "test@gmail.com">] gets converted to an object {name: "Hadi", email: "test@gmail.com"}
+ */
 function paramsMapToObject(paramsMap?: Map<string, any>) : any {
     const params: {[key: string]: any} = {};
     if(paramsMap == undefined) return params;
