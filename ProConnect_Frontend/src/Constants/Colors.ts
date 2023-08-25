a9778f177935d8225e3c55c9a35428b45c05ecae
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-25 22:19:58 +0300

    Document formatting

--- ProConnect_Frontend/src/Constants/Colors.ts
+++ ProConnect_Frontend/src/Constants/Colors.ts
@@ -5,37 +5,39 @@
 /**
  * This interface defines the primary colors used in the various parts of the application.
  */
-interface ColorScheme {
-    background: string;
-    container: string;
-    highlight: string;
-    textColor: string;
-    star: string;
-  }
-  
-  const Light: ColorScheme = {
-    background: "#CFE8FB",
-    container: "#FFFFFF7D",
-    highlight: "#0F30F7",
-    textColor: "#000000",
-    star: "#F2AF0D",
-  };
-  
-  const Dark: ColorScheme = {
-    background: "#222222",
-    container: "#5353537d",
-    highlight: "#7415c2",
-    textColor: "#FFFFFF",
-    star: "#F2AF0D",
-  };
-  
-  const Themes: Record<string, ColorScheme> = { Light, Dark };
-  
-  /**
-   * @returns ColorScheme based on the desired theme.
-   */
-  function getColors(theme: string = 'Light'): ColorScheme {
-    return Themes[theme] || Light; // Default to Light theme if the specified theme doesn't exist
-  }
-  
-  export default getColors;  
+interface ColorScheme
+{
+  background: string;
+  container: string;
+  highlight: string;
+  textColor: string;
+  star: string;
+}
+
+const Light: ColorScheme = {
+  background: "#CFE8FB",
+  container: "#FFFFFF7D",
+  highlight: "#0F30F7",
+  textColor: "#000000",
+  star: "#F2AF0D",
+};
+
+const Dark: ColorScheme = {
+  background: "#222222",
+  container: "#5353537d",
+  highlight: "#7415c2",
+  textColor: "#FFFFFF",
+  star: "#F2AF0D",
+};
+
+const Themes: Record<string, ColorScheme> = { Light, Dark };
+
+/**
+ * @returns ColorScheme based on the desired theme.
+ */
+function getColors(theme: string = 'Light'): ColorScheme
+{
+  return Themes[theme] || Light; // Default to Light theme if the specified theme doesn't exist
+}
+
+export default getColors;  
--- ProConnect_Frontend/src/Services/Requests.ts
+++ ProConnect_Frontend/src/Services/Requests.ts
@@ -4,51 +4,55 @@ const baseUrl = 'http://localhost:8080';
 
 /**
  * Get data from the server.
- * @param url URL mapping for the required API endpoint. For example, users/getUser
+ * @param apiEndpoint Mapping for the requested API endpoint. For example, users/getUser
  * @param paramsMap Map containing optional parameters that get sent in the URL, where the keys represent the parameter names, and the values are the parameter values.
  * For example: @example users/getUser?userId=14&name=Hadi
  * @returns A promise containing the result of the request.
  */
-function get(url:string, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>{
-    let response = axios({
-      method: 'get',
-      url: `${baseUrl}/${url}`,
-      params: paramsMapToObject(paramsMap),
-    });
-    return response;
+function get(apiEndpoint: string, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>
+{
+  let response = axios({
+    method: 'get',
+    url: `${baseUrl}/${apiEndpoint}`,
+    params: paramsMapToObject(paramsMap),
+  });
+  return response;
 }
 
 /**
  * Post data to the server.
- * @param url URL mapping for the required API endpoint. For example, users/updateUser
+ * @param apiEndpoint Mapping for the requested API endpoint. For example, users/updateUser
  * @param data JSON data that gets sent as the request body.
  * @param paramsMap Map containing optional parameters that get sent in the URL, where the keys represent the parameter names, and the values are the parameter values.
  * For example: @example users/updateUser?userId=14
  * @returns A promise containing the result of the request.
  */
-function post(url:string, data?: any, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>{
-    let response = axios({
-      method: 'post',
-      url: `${baseUrl}/${url}`,
-      params: paramsMapToObject(paramsMap),
-      headers: {'Content-Type': 'application/json'},
-      data: data
-    });
-    return response;
+function post(apiEndpoint: string, data?: any, paramsMap?: Map<string, any>): Promise<AxiosResponse<any, any>>
+{
+  let response = axios({
+    method: 'post',
+    url: `${baseUrl}/${apiEndpoint}`,
+    params: paramsMapToObject(paramsMap),
+    headers: { 'Content-Type': 'application/json' },
+    data: data
+  });
+  return response;
 }
 
 /**
  * Converts a parameter map into an object that can be used as request parameters. @example
  * A map containing [<"name", "Hadi">, <"email", "test@gmail.com">] gets converted to an object {name: "Hadi", email: "test@gmail.com"}
  */
-function paramsMapToObject(paramsMap?: Map<string, any>) : any {
-    const params: {[key: string]: any} = {};
-    if(paramsMap == undefined) return params;
+function paramsMapToObject(paramsMap?: Map<string, any>): any
+{
+  const params: { [key: string]: any } = {};
+  if (paramsMap == undefined) return params;
 
-    paramsMap.forEach((value, key) => {
-        params[key] = value;
-    });
-    return params;
+  paramsMap.forEach((value, key) =>
+  {
+    params[key] = value;
+  });
+  return params;
 }
 
-export {get, post};
+export { get, post };
