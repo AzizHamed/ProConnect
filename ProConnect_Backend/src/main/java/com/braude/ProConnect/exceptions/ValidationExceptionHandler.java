9381a92dc67900b1d3eb1c9b947d0ab22324db6b
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-31 03:43:25 +0300

    Custom validation exception handling

---
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/exceptions/ValidationExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.braude.ProConnect.exceptions;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<String> errorMessages = new ArrayList<>();
+        for (ConstraintViolation violation : ex.getConstraintViolations()) {
+            errorMessages.add(violation.getMessage());
+        }
+        return ResponseEntity.badRequest().body(errorMessages);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<Object> handleQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex) {
+        String message = ex.getMessage();
+        if (message.toLowerCase().contains("duplicate"))
+            return ResponseEntity.badRequest().body("Data already exists!");
+        return ResponseEntity.badRequest().body(message);
+
+    }
+}
