07fb76b7afdad7902a70457d24609d151a91f464
hadiDanial <hadiDanial@users.noreply.github.com>
2023-08-31 03:48:25 +0300

    Created Location class

---
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/models/embeddables/Geolocation.java
@@ -0,0 +1,37 @@
+package com.braude.ProConnect.models.embeddables;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Geolocation {
+    private double latitude;
+    private double longitude;
+
+    public Geolocation() {
+    }
+
+    public Geolocation(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Geolocation that = (Geolocation) o;
+        return Double.compare(that.latitude, latitude) == 0 && Double.compare(that.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + latitude + ", " + longitude +")";
+    }
+}
