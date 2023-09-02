deb0e143ff2ec54be432e4b1065c6cb3bae0d690
hadiDanial <hadiDanial@users.noreply.github.com>
2023-09-03 02:18:37 +0300

    Remove Skill and UserSkill

--- ProConnect_Backend/src/main/java/com/braude/ProConnect/repositories/SkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.braude.ProConnect.repositories;
-
-import com.braude.ProConnect.models.entities.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-}
--- ProConnect_Backend/src/main/java/com/braude/ProConnect/repositories/UserSkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.braude.ProConnect.repositories;
-
-import com.braude.ProConnect.models.entities.UserSkill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
-}
