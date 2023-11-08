bb826a4d7c82409edfb6d86eb4d158fe8c9a0273
hadiDanial <hadiDanial@users.noreply.github.com>
2023-11-08 20:06:27 +0200

    Update ProButton.tsx

--- ProConnect_Frontend/src/Components/Controls/ProButton.tsx
+++ ProConnect_Frontend/src/Components/Controls/ProButton.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { Button, ButtonProps } from 'react-native-ui-lib';
 
 interface ProButtonProps {
