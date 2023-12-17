b258a1f3e5963333558eab69ff0692a61b8a2671
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-17 20:18:33 +0200

    Update Store.ts

--- ProConnect_Frontend/src/Services/Store.ts
+++ ProConnect_Frontend/src/Services/Store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import JobReducer from "../Features/Jobs/JobSlice";
+import DimensionReducer from "./Redux/DimensionSlice";
+import PreferencesReducer, { initializePreferences } from "./Redux/PreferencesSlice";
 
 export const store = configureStore({
     reducer: {
-        job: JobReducer
+        job: JobReducer,
+        dimension: DimensionReducer,
+        preferences: PreferencesReducer
     }
 })
+
+store.dispatch(initializePreferences());
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
