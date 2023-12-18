ffb88d49ff2a7a13e5ed9e2b251abbcd95bb1fb4
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-18 03:44:43 +0200

    Create useViewPort to get responsive screen width and calculate desired width

---
+++ ProConnect_Frontend/src/Hooks/useViewPort.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Dimensions, ScaledSize, Animated, Platform } from "react-native";
+import { setDimensions, getWindow } from "../Services/Redux/DimensionSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../Services/Store";
+interface UpdateDimensionsData{
+  window: ScaledSize,
+  screen: ScaledSize
+}
+export function useViewport() {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  
+    const dispatch = useDispatch<AppDispatch>();
+    const currentWindow = useSelector(getWindow);
+
+    function updateDimensions({ window, screen } : UpdateDimensionsData)  {
+    if (window.width !== currentWindow.width || window.height !== currentWindow.height)
+      dispatch(setDimensions({ sceen: screen, window: window }));
+    setScreenWidth(window.width);
+    }
+
+    useEffect(() => {
+      
+      const subscription = Dimensions.addEventListener("change", updateDimensions);
+      return () => subscription?.remove();
+    }, []);
+  
+    // Return the width so we can use it in our components
+    return { screenWidth };
+  }
+
+  export function calculateResponsiveWidth(screenWidth: number, autoAdjustWidth: boolean = true, div: number = 2.25,
+    smallScreenWebMargin: number = 40, smallScreenWidth = 720){
+      if(screenWidth == undefined) screenWidth = Dimensions.get("window").width;
+      let tempWidth: number | 'auto' | `${number}%` | Animated.AnimatedNode| null = "90%";
+      if(autoAdjustWidth){
+        if(Platform.OS === "web"){
+          tempWidth = screenWidth > smallScreenWidth ? screenWidth / div : screenWidth - smallScreenWebMargin;
+        }
+      }        
+      else{
+        tempWidth = screenWidth - smallScreenWebMargin;
+        console.log(tempWidth, screenWidth, smallScreenWebMargin)
+      }              
+      return tempWidth;
+  }
