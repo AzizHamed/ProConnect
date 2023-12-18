04b581af9f1ba962f3fa187869421cd5e3e99d09
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-18 03:58:42 +0200

    Basic Job Page

--- ProConnect_Frontend/src/Features/Jobs/JobPage.tsx
+++ ProConnect_Frontend/src/Features/Jobs/JobPage.tsx
@@ -1,33 +1,59 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
-import {
-  View,
-  Text,
-  Card,
-  CardProps
-} from "react-native-ui-lib";
+import { ScrollView, StyleSheet } from "react-native";
+import { View, Text, Card, CardProps } from "react-native-ui-lib";
 import { Job } from "../../Models/Job";
 import ProCard from "../../Components/Layout/ProCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getSelectedJob, selectJob } from "./JobSlice";
 import { useNavigation } from "@react-navigation/native";
+import ProHeader, { HeaderType } from "../../Components/Layout/ProHeader";
+import { formatDateString } from "../../Utility/Formatter";
+import BackgeoundView from "../../Components/Layout/BackgroundView";
 
-interface JobPageProps {
-  job?: Job;
+function generateCardChildren(job: Job | null) {
+  if (job == null)
+    return (
+      <View>
+        <ProHeader text="No job selected." />
+      </View>
+    );
+  return (
+    <View>
+      <ProHeader text={job.title} marginB-30 headerType={HeaderType.H1} />
+      <Text>{job.description}</Text>
+      <Text t2 textAlign="right" marginT-20 style={styles.date}>
+        {formatDateString(job.datePosted)}
+      </Text>
+    </View>
+  );
 }
 
-const JobCard: React.FC<JobPageProps> = (props) => {
+const JobCard: React.FC = () => {
   const job = useSelector(getSelectedJob);
   const navigation = useNavigation();
+  const children = generateCardChildren(job);
+
   const dispatch = useDispatch();
-  useEffect(
-    ()=>{
-      navigation.addListener("beforeRemove", e=>{
-        dispatch(selectJob(null))
-      }), [navigation]
-    }
-  )
-  return (<View><Text>{job?.title}</Text></View>)
+  useEffect(() => {
+    navigation.addListener("beforeRemove", (e) => {
+      dispatch(selectJob(null));
+    }),
+      [navigation];
+  });
+  return (
+    <BackgeoundView children={(
+      <ScrollView>
+      <View style={styles.cardContainer} bg>
+        <ProCard children={children}></ProCard>
+        <ProCard children={(
+          <ProHeader text="Comments" headerType={HeaderType.H2}/>
+          )}></ProCard>
+        {/* textContent={content} */}
+      </View>
+    </ScrollView>
+    )}/>
+    
+  );
 };
 
 export default JobCard;
@@ -35,9 +61,15 @@ export default JobCard;
 const styles = StyleSheet.create({
   date: {
     alignSelf: "flex-end",
-    verticalAlign: "bottom"
+    verticalAlign: "bottom",
   },
-  card:{
-    width: '100%',
+  cardContainer: {
+    alignContent: "center",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignSelf: "center",
+    flex: 1,
+    width: "100%",
+    height: "100%",
   }
 });
