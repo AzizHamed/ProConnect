41ed1b47e34d41b0f472b79d88602aea7b78669a
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-18 03:49:49 +0200

    Added responsive width to ProCard

--- ProConnect_Frontend/src/Components/Layout/ProCard.tsx
+++ ProConnect_Frontend/src/Components/Layout/ProCard.tsx
@@ -1,63 +1,68 @@
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
-import { Text, View, Card, CardProps, Colors, CardSelectionOptions, TextProps, Assets } from 'react-native-ui-lib';
+import React, { useState } from "react";
+import {
+  View,
+  Card,
+  CardProps,
+  CardSelectionOptions,
+  TextProps,
+} from "react-native-ui-lib";
+import { calculateResponsiveWidth, useViewport } from "../../Hooks/useViewPort";
 
 interface ProCardProps {
-    radius?: number
-    onPress?: () => void
-    canBeSelected?: boolean
-    children: React.ReactNode
-    textContent?: CardSectionContent[]
+  radius?: number;
+  onPress?: () => void;
+  canBeSelected?: boolean;
+  autoAdjustWidth?: boolean;
+  children: React.ReactNode;
+  textContent?: CardSectionContent[];
 }
 
 export type CardSectionContent = TextProps & {
-  text: string
-}
+  text: string;
+};
 
 const ProCard: React.FC<ProCardProps & CardProps> = (props) => {
-    const canBeSelected = props.canBeSelected || false;
-    const children = props.children || <></>;
-    const textContent = props.textContent || []
-    const borderRadius = props.radius || 5;
-    const selectionOptions: CardSelectionOptions = {
-      icon: 0, 
-      borderWidth: 3,
-      hideIndicator: true,
-      indicatorSize: 0
+  const [isSelected, setIsSelected] = useState(false);
+  const { screenWidth } = useViewport();
+  const canBeSelected = props.canBeSelected || false;
+  const children = props.children || <></>;
+  const textContent = props.textContent || [];
+  const borderRadius = props.radius || 5;
+  const autoAdjustWidth = props.autoAdjustWidth || false;
+
+  const selectionOptions: CardSelectionOptions = {
+    icon: 0,
+    borderWidth: 3,
+    hideIndicator: !canBeSelected,
+    indicatorSize: canBeSelected ? 5 : 0,
+  };
+
+  const onPress = props.onPress !== undefined ? props.onPress
+      : () => { console.log("Pressed"); };
+
+  function calculateWidth() { 
+    return calculateResponsiveWidth(screenWidth, autoAdjustWidth);
   }
-    const [isSelected, setIsSelected] = useState(false)
-    const onPress = props.onPress !== undefined ? props.onPress : () =>
-    {
-        console.log("Pressed")
-    }
 
   return (
-        <Card
-          flex margin-20
-          selected={isSelected}
-          selectionOptions={selectionOptions}
-          onPress={() => {
-            if(canBeSelected)
-                setIsSelected(!isSelected)
-            onPress();
-          }}
-          
-          borderRadius={borderRadius}
-          padding-20
-        >
-          {/* <Card.Image source={{uri: Assets.images.gradient}} height={200} cover ></Card.Image> */}
-          {textContent.map((content)=> ( (<Card.Section content={[{...content}]}/> ) ))}
-          <View>
-            {children}
-          </View>
-        </Card>
-  )
-}
+    <Card
+      style={{ width: calculateWidth() }}
+      selected={isSelected}
+      selectionOptions={selectionOptions}
+      onPress={() => {
+        if (canBeSelected) setIsSelected(!isSelected);
+        onPress();
+      }}
+      borderRadius={borderRadius}
+      padding-20
+    >
+      {/* <Card.Image source={{uri: Assets.images.gradient}} height={200} cover ></Card.Image> */}
+      {textContent.map((content) => (
+        <Card.Section content={[{ ...content }]} />
+      ))}
+      <View>{children}</View>
+    </Card>
+  );
+};
 
-export default ProCard
-
-const styles = StyleSheet.create({
-  date: {
-    alignSelf: 'flex-end'
-  },
-});
+export default ProCard;
