5d2a99762b26de665ac3caf03c71d1d60a165d59
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-19 14:39:54 +0200

    Update Formatter.ts

--- ProConnect_Frontend/src/Utility/Formatter.ts
+++ ProConnect_Frontend/src/Utility/Formatter.ts
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 
-export function formatDateString(dateString: string) : string{
+export function formatDateString(dateString?: string) : string{
+    if(dateString == undefined) return "Undefined Date";
     var date = new Date(dateString);
     var formattedDate = format(date, "yyyy-MM-d H:mma");
     return formattedDate;
