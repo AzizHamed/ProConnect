85f8931f1a5fec1b5909f89a1b91abf956b071c9
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-19 14:42:00 +0200

    Updated JobsList to use RTK Query, added Loading/Error component

--- ProConnect_Frontend/src/Features/Jobs/JobsList.tsx
+++ ProConnect_Frontend/src/Features/Jobs/JobsList.tsx
@@ -1,38 +1,47 @@
 import { View } from "react-native-ui-lib";
 import { ScrollView, StyleSheet } from "react-native";
 import JobCard from "./JobCard";
-import { useGetJobs } from "./Hooks/useGetJobs";
 import ProButton from "../../Components/Controls/ProButton";
 import { useNavigation } from "@react-navigation/native";
-import { useSelector } from "react-redux";
-import { getJobs } from "./JobSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getJobs, setJobs } from "./JobSlice";
 import BackgroundView from "../../Components/Layout/BackgroundView";
+import { useGetJobsQuery } from "../../Services/Redux/Api";
+import { useEffect } from "react";
+import LoadingOrError from "../../Components/Layout/LoadingOrError";
 
 // TODO: Filter by budget, job status, JobDateSearch
 
 const JobsList: React.FC = () => {
-  useGetJobs();
+  const { data, isSuccess, isError } = useGetJobsQuery({});
   const navigation = useNavigation();
   const jobs = useSelector(getJobs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Update the jobs data in the Redux store every time the data changes
+    dispatch(setJobs((data !== undefined && data.content !== undefined) ? data.content : []))
+  }, [data])
+
   return (
-    // <View style={styles.background} bg>
     <BackgroundView
       children={
         <ScrollView>
           <ProButton isResponsive onPress={() => {
               navigation.navigate("Testing");
             }}/>
-          <View style={styles.container} bg>
+
+            <LoadingOrError isSuccess={isSuccess} isError={isError} errorDisplayMessage/>
+
+            {isSuccess &&  <View style={styles.container} bg>
             {jobs.map((job) => {
               return <JobCard autoAdjustWidth key={job.id} job={job}></JobCard>;
             })}
-            {/* {jobs.map((job) => { return <JobCard key={job.id} job={job}></JobCard> })}         */}
-            {/* {jobs.map((job) => { return <JobCard key={job.id} job={job}></JobCard> })}         */}
-          </View>
+            </View>
+            }
         </ScrollView>
       }
     />
-
   );
 };
 
