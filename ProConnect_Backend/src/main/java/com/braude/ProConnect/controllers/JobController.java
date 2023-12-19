21323b3313ddab7a3eb89fc5e37150f68a460623
hadiDanial <hadiDanial@users.noreply.github.com>
2023-12-19 14:37:13 +0200

    Update JobController.java

--- ProConnect_Backend/src/main/java/com/braude/ProConnect/controllers/JobController.java
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/controllers/JobController.java
@@ -1,23 +1,18 @@
 package com.braude.ProConnect.controllers;
 
 
-import com.braude.ProConnect.exceptions.ProConnectException;
 import com.braude.ProConnect.models.entities.Comment;
 import com.braude.ProConnect.models.entities.Job;
-import com.braude.ProConnect.models.entities.User;
 import com.braude.ProConnect.models.page.JobPage;
 import com.braude.ProConnect.models.searchCriteria.JobSearchCriteria;
 import com.braude.ProConnect.services.JobService;
 import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("jobs")
@@ -40,8 +35,8 @@ public class JobController {
     }
 
 
-    @GetMapping("/page")
-    public ResponseEntity<Page<Job>> getPages(JobPage jobPage, JobSearchCriteria jobSearchCriteria){
+    @GetMapping("/get-job-page")
+    public ResponseEntity<Page<Job>> getJobs(JobPage jobPage, JobSearchCriteria jobSearchCriteria){
         return new ResponseEntity<>(jobService.getJobs(jobPage,jobSearchCriteria),
                 HttpStatus.OK);
     }
@@ -52,7 +47,7 @@ public class JobController {
     }
 
     @PutMapping("/unlike")
-    public String unLikePost(@RequestParam @Nonnull Long jobId, @RequestParam @Nonnull Long userId){
+    public String unlikePost(@RequestParam @Nonnull Long jobId, @RequestParam @Nonnull Long userId){
         return jobService.unLikePost(jobId,userId);
     }
 
