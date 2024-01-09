65e1f02de0d56506c7a1047a8f2ed0e88c07ff28
hadiDanial <hadiDanial@users.noreply.github.com>
2024-01-09 15:20:59 +0200

    Add React Native dotenv

--- ProConnect_Frontend/babel.config.js
+++ ProConnect_Frontend/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      // Other plugins...
+      ["module:react-native-dotenv",]
     ],
   };
 };
