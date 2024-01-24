1037a925c028082fcba4b8a88ad15d639a461634
hadiDanial <hadiDanial@users.noreply.github.com>
2024-01-24 18:48:50 +0200

    Rename @env BASE_URL to REACT_APP_BASE_URL

--- ProConnect_Frontend/src/Services/Redux/EmptyApi.ts
+++ ProConnect_Frontend/src/Services/Redux/EmptyApi.ts
@@ -1,10 +1,10 @@
 // Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { BASE_URL } from "@env"
+import { REACT_APP_BASE_URL } from "@env"
 
-const url = BASE_URL;
+const url = REACT_APP_BASE_URL;
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: url }),
   endpoints: () => ({}),
 })
