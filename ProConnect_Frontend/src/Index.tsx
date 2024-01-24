fad921fc5b75ae80578be2f22f4bb34a4d48aac9
hadiDanial <hadiDanial@users.noreply.github.com>
2024-01-25 00:48:21 +0200

    Update Index.tsx

--- ProConnect_Frontend/src/Index.tsx
+++ ProConnect_Frontend/src/Index.tsx
@@ -12,6 +12,7 @@ import Testing from "./Components/Testing";
 import JobPage from "./Features/Jobs/JobPage";
 import { setDimensions } from "./Services/Redux/DimensionSlice";
 import { AppDispatch } from "./Services/Store";
+import EmailPasswordLoginScreen from "./Screens/Login/EmailPasswordLoginScreen";
 
 const Index: React.FC = () => {
   const Stack = createNativeStackNavigator();
@@ -32,6 +33,7 @@ const Index: React.FC = () => {
   return (
       <NavigationContainer key={darkTheme.toString()}>
         <Stack.Navigator>
+          <Stack.Screen name="Login" component={EmailPasswordLoginScreen} />
           <Stack.Screen name="Home" component={JobsList} />
           <Stack.Screen name="Testing" component={Testing} />
           <Stack.Screen name="Job" component={JobPage} />
