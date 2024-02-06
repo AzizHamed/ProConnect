49692091abc943fb8ddd4a9f7aaed57f749887bd
Hadi <hadidanial97@gmail.com>
2024-02-06 11:50:48 +0200

    Add Auth Header to RTK Query requests
    Get auth idToken from Redux Auth store and add it to the headers

--- ProConnect_Frontend/src/Services/Redux/EmptyApi.ts
+++ ProConnect_Frontend/src/Services/Redux/EmptyApi.ts
@@ -1,10 +1,16 @@
 // Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { REACT_APP_BASE_URL } from "@env"
-
 const url = REACT_APP_BASE_URL;
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: url }),
+  baseQuery: fetchBaseQuery({ baseUrl: url, 
+    prepareHeaders: (headers, {getState}) => {
+      const state = getState();
+      const idToken = state?.auth?.idToken;
+      if(idToken && idToken !== '')
+        headers.set('Authorization', idToken);
+    }
+    }),
   endpoints: () => ({}),
 })
