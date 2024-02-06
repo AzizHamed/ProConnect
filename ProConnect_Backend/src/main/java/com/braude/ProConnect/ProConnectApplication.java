167f31aab3023eba6acefecda4dddb2a6c2ea13f
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-07 01:29:45 +0200

    Update ProConnectApplication.java

--- ProConnect_Backend/src/main/java/com/braude/ProConnect/ProConnectApplication.java
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/ProConnectApplication.java
@@ -10,8 +10,6 @@ import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
