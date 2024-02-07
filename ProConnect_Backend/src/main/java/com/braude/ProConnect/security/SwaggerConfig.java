afc55d8fb607e7edca3cc95bc3505f69eea0ef7e
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-07 11:37:41 +0200

    Swagger Security Config

---
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/security/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.braude.ProConnect.security;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("ProConnect API")
+                        .version(appVersion)
+                        .description("This is the ProConnect API.\nProConnect is the final project at Braude College of Engineering by Hadi and Aziz.\n" +
+                                "You can find the source code at [https://github.com/hadiDanial/ProConnect](https://github.com/hadiDanial/ProConnect).\n" +
+                                "You must provide a valid Firebase authentication access token in order to use this API."));
+    }
+}
