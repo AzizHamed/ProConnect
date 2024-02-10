926ef76e68ec4bb68a1e18e37a6ce5c91750a226
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-11 00:21:51 +0200

    Create annotation-based OpenAPI config (Swagger)

---
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/security/OpenAPIConfig.java
@@ -0,0 +1,40 @@
+package com.braude.ProConnect.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/***
+ * OpenAPI configuration. Configures the Swagger UI information and security schemes.
+ */
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Hadi Danial | Aziz Hamed",
+                        url = "https://github.com/hadiDanial/proconnect",
+                        email = "hadidanial97@gmail.com"
+                ),
+                title = "ProConnect API",
+                description = "This is the ProConnect API.\nProConnect is the final project at Braude College of Engineering by Hadi and Aziz.\n" +
+                        "You can find the source code at [https://github.com/hadiDanial/ProConnect](https://github.com/hadiDanial/ProConnect).\n" +
+                        "You must provide a valid Firebase authentication access token in order to use this API.",
+                version = "0.3"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+
+)
+@SecurityScheme(name = "bearerAuth",
+        description = "Firebase Auth IdToken",
+        scheme = "basic",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig { }
