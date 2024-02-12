e69cb6947844ea5a5a82ef36ea2d2e92fdddfbd8
Hadi <hadidanial97@gmail.com>
2024-02-12 13:45:20 +0200

    Add ProfileEditor page

--- ProConnect_Frontend/src/Navigation/AppStack.tsx
+++ ProConnect_Frontend/src/Navigation/AppStack.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { MainDrawerScreen } from "./MainDrawerScreen";
 import { AuthStackScreen } from "./AuthStack";
+import ProfileEditorScreen from "../Screens/Profile/ProfileEditorScreen";
 
 const AppStack = createNativeStackNavigator();
 
@@ -9,6 +10,15 @@ export const AppStackScreen: React.FC = () => {
     <AppStack.Navigator screenOptions={{headerShown: false}}>
         <AppStack.Screen name="Auth" component={AuthStackScreen}/>
         <AppStack.Screen name="Main" component={MainDrawerScreen} />
+
+
+
+
+
+
+
+
+        <AppStack.Screen name="ProfileEditor" component={ProfileEditorScreen}/>
     </AppStack.Navigator>
   );
 };
