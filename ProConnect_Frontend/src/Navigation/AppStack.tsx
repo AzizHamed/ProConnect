648c498d202d1e5d93cd7b75bf8f9c5f1d68e573
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-14 15:57:18 +0200

    Fix merge issues

--- ProConnect_Frontend/src/Navigation/AppStack.tsx
+++ ProConnect_Frontend/src/Navigation/AppStack.tsx
@@ -3,7 +3,6 @@ import { MainDrawerScreen } from "./MainDrawerScreen";
 import { AuthStackScreen } from "./AuthStack";
 import JobsList from "../Features/Jobs/JobsList";
 import JobPage from "../Features/Jobs/JobPage";
-import Testing from "../Components/Testing";
 import ProfessionalsPage from "./OldNav-TabsAndSideBars/ProfessionalsPage";
 
 
@@ -16,7 +15,6 @@ export const AppStackScreen: React.FC = () => {
         <AppStack.Screen name="Main" component={MainDrawerScreen} />
 
         <AppStack.Screen options={{headerShown: true}}  name="Job" component={JobPage}  />
-        <AppStack.Screen name="Testing" component={Testing}  />
         <AppStack.Screen name="ProfessionalsPage" component={ProfessionalsPage}  />
 
         
