e48f8bcc90431b494c9aa38c51c7329e9df65195
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-21 01:20:16 +0200

    Added BackgroundView scroll option, height now fits with tab bar height

--- ProConnect_Frontend/src/Components/Layout/BackgroundView.tsx
+++ ProConnect_Frontend/src/Components/Layout/BackgroundView.tsx
@@ -1,14 +1,31 @@
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native-ui-lib";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useSelector } from "react-redux";
+import { getScreenHeight } from "../../Services/Redux/Slices/DimensionSlice";
 
 interface BackgroundViewProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    hasScroll?: boolean
 }
 
 const BackgroundView: React.FC<BackgroundViewProps> = (props) => {
+  const tabBarHeight = useBottomTabBarHeight();
+  const screenHeight = useSelector(getScreenHeight);
+
+  if(props.hasScroll) {
+    return(
+      <SafeAreaView style={[styles.background, {height: screenHeight - tabBarHeight}]}>
+        <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+          <View style={styles.background} bg>{props.children}</View>
+        </ScrollView>
+    </SafeAreaView>
+    );
+  }
+
   return(
-    <SafeAreaView style={styles.background}>
+    <SafeAreaView style={[styles.background, {height: screenHeight - tabBarHeight}]}>
       <View style={styles.background} bg>{props.children}</View>
     </SafeAreaView>
     );
@@ -19,7 +36,6 @@ export default BackgroundView;
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    height: Dimensions.get("window").height,
     width: "100%"
-  },
+    },
 });
