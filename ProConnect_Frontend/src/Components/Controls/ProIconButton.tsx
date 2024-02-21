ebebb5204a690b1b5f22fc3ec9d243f55b028095
hadiDanial <hadiDanial@users.noreply.github.com>
2024-02-22 01:13:53 +0200

    Update ProIconButton.tsx

--- ProConnect_Frontend/src/Components/Controls/ProIconButton.tsx
+++ ProConnect_Frontend/src/Components/Controls/ProIconButton.tsx
@@ -71,6 +71,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginHorizontal: 5,
+        backgroundColor: Colors.controlBackground,
     },
     plus: {
         position: 'absolute',
