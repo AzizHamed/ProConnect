7261e1b6306e6eb446ba6eff59e1bef9c360e46b
AzizHamed <Abd.Elaziz.Hamed@e.braude.ac.il>
2024-02-25 22:58:21 +0200

    add image to chats

--- ProConnect_Frontend/src/Navigation/AppStack.tsx
+++ ProConnect_Frontend/src/Navigation/AppStack.tsx
@@ -6,8 +6,11 @@ import JobPage from "../Features/Jobs/JobPage";
 import PersonsPage from "../Features/Persons/PersonsPage";
 import Chat from "../Screens/Chat/Chat";
 
+import {Image, StyleSheet, View,Text} from 'react-native'
+
 
 const AppStack = createNativeStackNavigator();
+
 
 export const AppStackScreen: React.FC = () => {
   return (
@@ -17,9 +20,31 @@ export const AppStackScreen: React.FC = () => {
 
         <AppStack.Screen options={{headerShown: true}}  name="Job" component={JobPage}  />
         <AppStack.Screen name="PersonsPage" component={PersonsPage} />
-        <AppStack.Screen name="Chats" component={Chat} options={{headerShown : true}} />
+        <AppStack.Screen name="Chats" component={Chat} options={{headerShown : true, headerTitle: () => (
+            <View style={styles.chatHeader}>
+                <Image
+                source={require('../../gardner2.png')}
+                style={{ width: 40, height: 40,  borderRadius : 40}}
+              />
+
+              <View>
+              <Text>Aziz Hamed</Text>
+              <Text> Available</Text>
+              </View>
+            </View>
+            ),}} />
 
     </AppStack.Navigator>
 
   );
 };
+
+const styles = StyleSheet.create({
+  chatHeader : {
+    flexDirection : "row",
+   justifyContent : "space-between",
+   width : 130,
+   alignItems : "center",
+
+  },
+})
