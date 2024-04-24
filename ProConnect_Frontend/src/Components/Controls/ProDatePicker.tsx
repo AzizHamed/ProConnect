bf1e78e7397198f0e77ef242f4051698a62813f7
hadiDanial <hadiDanial@users.noreply.github.com>
2024-04-24 20:30:41 +0300

    Update ProDatePicker.tsx

--- ProConnect_Frontend/src/Components/Controls/ProDatePicker.tsx
+++ ProConnect_Frontend/src/Components/Controls/ProDatePicker.tsx
@@ -10,7 +10,8 @@ interface ProDatePickerProps {
     control: any,
     name: string,
     placeholder?: string,
-    setValue: (value: Date) => void,
+    setDateValue: (value: Date) => void,
+    setFormattedDateString?: (value: string) => void,
 }
 
 const ProDatePicker: React.FC<ProDatePickerProps> = (props) => {
@@ -18,7 +19,8 @@ const ProDatePicker: React.FC<ProDatePickerProps> = (props) => {
     const [date, setDate] = useState(new Date(Date.now()));
     const [showDatePicker, setShowDatePicker] = useState(false);
     const style = { color: Colors.black, fontSize: 16, height: 45, borderColor: Colors.controlText, borderWidth: 1, borderRadius: 50 };
-    const dateTitle = props.name !== undefined ? <Text marginB-10>{props.name}</Text> : <></>; 
+    const dateTitle = props.name !== undefined ? <Text marginB-10 style={{width:"100%"}}>{props.name}</Text> : <></>; 
+    
     // format the date to dd-mm-yyyy
     const formatDate = (date: Date) => {
         return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
@@ -33,18 +35,19 @@ const ProDatePicker: React.FC<ProDatePickerProps> = (props) => {
             if (event.type === 'set') {
                 const currentDate = selectedDate || date;
                 setDate(currentDate);
-                props.setValue(date);
+                props.setDateValue(date);
+                if(props.setFormattedDateString) props.setFormattedDateString(formatDate(currentDate)); 
             }
         }, 10);
     };
     if (isWeb) {
         return (
-            <View>
+            <View style={{width:"100%"}}>
                 {dateTitle}
                 {createElement('input', {
                     type: 'date',
                     value: date.toISOString().split("T")[0],
-                    onChange: (event) => {
+                    onChange: (event: any) => {
                         setDate(new Date(event.target.value))
                     },
                     style: { marginTop: 5, padding: 5, paddingLeft: 20, border: "1px solid #677788", ...style }
@@ -56,8 +59,8 @@ const ProDatePicker: React.FC<ProDatePickerProps> = (props) => {
         <View>
             {dateTitle}
             <TouchableOpacity onPress={() => { if (!showDatePicker) toggleDatePicker(); }}>
+                <TextInput style={[style, { paddingLeft: 60, backgroundColor: 'white' }]} editable={false} placeholder={props.placeholder} value={date.toDateString()} />
                 <Ionicons name="calendar" size={24} style={{ position: 'absolute', top: 10, left: 20 }} color={Colors.textPrimary} />
-                <TextInput style={[style, { paddingLeft: 60 }]} editable={false} placeholder={props.placeholder} value={date.toDateString()} />
             </TouchableOpacity>
             {showDatePicker && <RNDateTimePicker value={date} mode='date' display='spinner' onChange={onChange} />}
         </View>
