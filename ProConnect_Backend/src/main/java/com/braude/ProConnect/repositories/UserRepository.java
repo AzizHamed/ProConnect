7f59733b1c12a20f935eba18afa4ec526533a8ae
AzizHamed <Abd.Elaziz.Hamed@e.braude.ac.il>
2024-04-25 18:53:37 +0300

    fix my backend

--- ProConnect_Backend/src/main/java/com/braude/ProConnect/repositories/UserRepository.java
+++ ProConnect_Backend/src/main/java/com/braude/ProConnect/repositories/UserRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, String> {
 
 
-    User findByEmail(String email);
-
-
-    List<User> findAllByEmailList(List<String> emails);
+//    User findByEmail(String email);
+//
+//
+//    List<User> findAllByEmailList(List<String> emails);
 
 }
