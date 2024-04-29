f04fdb7d86475ba215a122d517e7d5adb9d4375e
hadiDanial <hadiDanial@users.noreply.github.com>
2024-04-29 04:04:17 +0300

    Update StartupScreen.tsx

--- ProConnect_Frontend/src/Screens/Authentication/StartupScreen.tsx
+++ ProConnect_Frontend/src/Screens/Authentication/StartupScreen.tsx
@@ -11,97 +11,102 @@ import { api, CreateUserApiArg, GetUserApiArg, User } from '../../Services/Redux
 import { Text } from 'react-native-ui-lib'
 import ProButton from '../../Components/Controls/ProButton'
 
-const StartupScreen:React.FC = () => {
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
-    // const userCredentials = useSelector();
-    useEffect(() => {
-      const onAuthChanged = onAuthStateChanged(webAuth, (firebaseUser) => {
+const StartupScreen: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  let authTokenRefreshInterval: NodeJS.Timeout | null = null;
+  // const userCredentials = useSelector();
+  useEffect(() => {
+    const onAuthChanged = onAuthStateChanged(webAuth, (firebaseUser) => {
+
+      authTokenRefreshInterval = setInterval(() => {
+        console.log('Refreshing Auth Token');
+        updateAuthState();
+      }, 1000 * 60 * 50);
+      updateAuthState()
+
+      function updateAuthState() {
         if (firebaseUser !== null && firebaseUser !== undefined) {
           firebaseUser?.getIdToken(true).then(async (idToken: any) => {
-              const userDetails: UserDetails = {
-                email: firebaseUser?.email || "",
-                name: firebaseUser?.displayName || "",
-                phone: firebaseUser?.phoneNumber || "",
-                idToken: idToken,
-                uid: firebaseUser?.uid,
-                photoURL: firebaseUser?.photoURL || "",
-              };
-              dispatch(setUserCredential(userDetails));
-              const getUserArgs: GetUserApiArg = {
-                userId: userDetails.uid || "",
-              };
-              console.log('Get User Args:', getUserArgs)
-              const promise = dispatch(api.endpoints.getUser.initiate(getUserArgs));
+            const userDetails: UserDetails = {
+              email: firebaseUser?.email || "",
+              name: firebaseUser?.displayName || "",
+              phone: firebaseUser?.phoneNumber || "",
+              idToken: idToken,
+              uid: firebaseUser?.uid,
+              photoURL: firebaseUser?.photoURL || "",
+            }
+            dispatch(setUserCredential(userDetails))
+            const getUserArgs: GetUserApiArg = {
+              userId: userDetails.uid || "",
+            }
+            // console.log('Get User Args:', getUserArgs)
+            const userExistsPromise = dispatch(api.endpoints.userExists.initiate({ userId: userDetails.uid }));
+            const { data: userExists } = await userExistsPromise;
+            if (userExists) {
+              const promise = dispatch(api.endpoints.getUser.initiate(getUserArgs))
               const { data } = await promise;
-              const user : User = data as User;
-              console.log('User Details:', userDetails);
-              console.log('User:', user);
-              if(user === undefined) {
-                const userToCreate: CreateUserApiArg = {user: {
-                  id: firebaseUser.uid,
-                  email: firebaseUser.email || 'Error',
-                  name:{firstName: firebaseUser.displayName || '', lastName: ''},
-                  phoneNumber: firebaseUser.phoneNumber || '',
-                  accountStatus: 'SETUP',
-                  photoUrl: firebaseUser.photoURL || '',                  
-                },
-                  
-                };
-                console.log('User not found, creating new user', userToCreate);
-                const createUserPromise = dispatch(api.endpoints.createUser.initiate(userToCreate))
-                const {data: newUser} = await createUserPromise;
-                console.log('New User Data:', newUser);
-                dispatch(setUserAccount(newUser as User));
-                navigation.reset({
-                  index: 0,
-                  routes: [{ name: "Main" }],
-                });
-              }
-              else if (user.accountStatus === 'SETUP') {
-                // This is a new user, load profile creation screen
-                console.log('New User, loading profile editor')
-                dispatch(setUserAccount(user));
-                navigation.reset({
-                  index: 0,
-                  routes: [{ name: "Main" }],
-                });
-              }
-              else {
-                // The user is registered and past the Setup stage, load main screen
-                dispatch(setUserAccount(user));
-                // setTimeout(() => {
+              const user: User = data as User;
+                dispatch(setUserAccount(user))
+                setTimeout(() => {                  
                   navigation.reset({
                     index: 0,
                     routes: [{ name: "Main" }],
-                  });                  
-                // }, 100);
+                  })
+                }, 500);              
+            }
+            else {
+              const userToCreate: CreateUserApiArg = {
+                user: {
+                  id: firebaseUser.uid,
+                  email: firebaseUser.email || 'Error',
+                  name: { firstName: firebaseUser.displayName || '', lastName: '' },
+                  phoneNumber: firebaseUser.phoneNumber || '',
+                  accountStatus: 'SETUP',
+                  photoUrl: firebaseUser.photoURL || '',
+                }
               }
-            })
+              // console.log('User not found, creating new user', userToCreate);
+              const createUserPromise = dispatch(api.endpoints.createUser.initiate(userToCreate))
+              const { data: newUser } = await createUserPromise
+              // console.log('New User Data:', newUser);
+              dispatch(setUserAccount(newUser as User))
+              setTimeout(() => {                  
+                navigation.reset({
+                  index: 0,
+                  routes: [{ name: "Main" }],
+                })
+              }, 500);
+
+            }
+        })
             .catch((error: any) => {
-              console.log("Error getting id token of user", error);
-              dispatch(setUserCredential({}));
-            });
+              console.log("Error getting id token of user", error)
+              dispatch(setUserCredential({}))
+            })
         } else {
           // Handle the case when user is null or undefined
           // Display Sign In Screen
-          navigation.navigate('Login');
+          navigation.navigate('Login')
         }
-      });
-      return () => {
-        onAuthChanged();
-      };
-    }, [])
-  
+      }
+    });
+    return () => {
+      if (authTokenRefreshInterval !== null)
+        clearInterval(authTokenRefreshInterval);
+      onAuthChanged();
+    };
+  }, [])
+
 
   return (
     <BackgroundView hasSafeAreaView children={(
-        <>
-            <ProHeader style={{marginTop: 75}} text='ProConnect' headerType={HeaderType.H1} center />
-            <Text center marginT-50>Loading user data...</Text>
-            <ProLoading displayLoadingMessage={false}/>
-            <ProButton onPress={emailSignOut} text={'Cancel'} center></ProButton>
-        </>
+      <>
+        <ProHeader style={{ marginTop: 75 }} text='ProConnect' headerType={HeaderType.H1} center />
+        <Text center marginT-50>Loading user data...</Text>
+        <ProLoading displayLoadingMessage={false} />
+        <ProButton onPress={emailSignOut} text={'Cancel'} center></ProButton>
+      </>
     )}></BackgroundView>
   )
 }
