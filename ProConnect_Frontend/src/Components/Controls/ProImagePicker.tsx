2d41ca3026c7d6d7e7da711af8e2eb43346098f2
hadiDanial <hadiDanial@users.noreply.github.com>
2024-05-04 20:58:58 +0300

    Update ProImagePicker.tsx

--- ProConnect_Frontend/src/Components/Controls/ProImagePicker.tsx
+++ ProConnect_Frontend/src/Components/Controls/ProImagePicker.tsx
@@ -29,7 +29,7 @@ const ProImagePicker: React.FC<ProImagePickerProps> = (props) => {
         <ProIconButton ionicon displayBackground ioniconName="camera" showAddIcon onPress={() => selectPictures('CAMERA', true)}></ProIconButton>
 
       </View>
-      <ProCarousel data={Array.from(selectedFiles || [])}  mode='parallax' width={isWeb ? 300 : undefined}
+      <ProCarousel data={Array.from(selectedFiles || [])}  mode='parallax' width={isWeb ? 300 : undefined} displayArrows arrowOffset={isWeb? undefined : 0}
         renderItems={({ item, index }) => (
           <View key={index}  style={{ flex: 1, borderWidth: 1, justifyContent: 'center' }} >
             <Image source={{ uri: item.uri }} style={{ width: '100%', height: '100%' }} resizeMode='contain' />
