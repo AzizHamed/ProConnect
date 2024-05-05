42944725c945afad496278c399d2c0a3b288590c
hadiDanial <hadiDanial@users.noreply.github.com>
2024-05-05 17:26:47 +0300

    Update AppStack.tsx

--- ProConnect_Frontend/src/Navigation/AppStack.tsx
+++ ProConnect_Frontend/src/Navigation/AppStack.tsx
@@ -6,33 +6,39 @@ import JobPage from "../Features/Jobs/JobPage";
 import PersonsPage from "../Features/Persons/PersonsPage";
 import Chat from "../Screens/Chat/Chat";
 
-import {Image, StyleSheet, View,Text, Dimensions} from 'react-native'
+import { Image, StyleSheet, View, Text, Dimensions } from 'react-native'
 import { Colors } from "react-native-ui-lib";
 import { color } from "react-native-elements/dist/helpers";
 import FullMapScreen from "../Screens/Chat/FullMapScreen";
-import { useSelector } from "react-redux";
-import { getSelectedReceiverUserName, getReceiverPhotoUrl } from "../Services/Redux/Slices/ChatSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getSelectedReceiverUserName, getReceiverPhotoUrl, getSelectedReceiverUser } from "../Services/Redux/Slices/ChatSlice";
 import ProButton from "../Components/Controls/ProButton";
 import { AirbnbRating } from "react-native-ratings";
 import ProfileImage from "../Components/Layout/ProfileImage";
+import ProfileEditorScreen from "../Screens/Profile/ProfileEditorScreen";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { selectUser } from "../Services/Redux/Slices/UserSlice";
+import { useNavigation } from "@react-navigation/native";
 
 
 const AppStack = createNativeStackNavigator();
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 export const AppStackScreen: React.FC = () => {
 
   const receiverName = useSelector(getSelectedReceiverUserName);
   const receiverPhotoUrl = useSelector(getReceiverPhotoUrl);
+  const user = useSelector(getSelectedReceiverUser);
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-  
 
- 
   return (
-    <AppStack.Navigator screenOptions={{headerShown: false}}>
-        <AppStack.Screen name="Auth" component={AuthStackScreen}/>
-        <AppStack.Screen name="Main" component={MainDrawerScreen} />
+    <AppStack.Navigator screenOptions={{ headerShown: false }}>
+      <AppStack.Screen name="Auth" component={AuthStackScreen} />
+      <AppStack.Screen name="Main" component={MainDrawerScreen} />
+      <AppStack.Screen name="NewUserProfile" component={ProfileEditorScreen} />
 
         <AppStack.Screen options={{headerShown: true}}  name="Job" component={JobPage}  />
         <AppStack.Screen name="PersonsPage" component={PersonsPage} options={{headerShown : true}}  />
@@ -40,34 +46,28 @@ export const AppStackScreen: React.FC = () => {
           <View style={styles.container}>
 
             <View style={styles.chatHeader}>
-              <ProfileImage size={40} photoUrl={receiverPhotoUrl}/>
-    
-            <View style={{width : "87%"}}>
-              {/* <View style={{paddingLeft : 2}}> */}
+              <TouchableOpacity onPress={()=>{
+                // dispatch(selectUser(user));
+                navigation.navigate("Profile");
+              }}>
 
-            <Text style={{color : "white"}}>{receiverName}</Text>
-              {/* </View> */}
-            {/* <View style={{ alignItems : "flex-start"}}>
+                <ProfileImage size={40} photoUrl={receiverPhotoUrl} />
 
-            <AirbnbRating
-              defaultRating={5}
-              count={5}
-              size={15}
-              isDisabled={true}
-              
-              showRating={false}  starContainerStyle={{marginRight : 2}}/>
-            </View> */}
+                <View style={{ width: "87%" }}>
+                  <Text style={{ color: "white" }}>{receiverName}</Text>
+                </View>
+              </TouchableOpacity>
             </View>
-          </View>
-        
-          </View>
-            ), headerRight: () => (
-              <View style={{ marginRight: 20 }}>
-                
-              </View>
-            ),}} />
 
-            <AppStack.Screen name="FullMapScreen" component={FullMapScreen} options={{headerShown : true,headerTintColor : "white", headerStyle :styles.header}} />
+          </View>
+        ), headerRight: () => (
+          <View style={{ marginRight: 20 }}>
+
+          </View>
+        ),
+      }} />
+
+      <AppStack.Screen name="FullMapScreen" component={FullMapScreen} options={{ headerShown: true, headerTintColor: "white", headerStyle: styles.header }} />
 
     </AppStack.Navigator>
 
@@ -75,18 +75,18 @@ export const AppStackScreen: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  chatHeader : {
-    flexDirection : "row",
-   justifyContent : "space-between",
-   alignItems : "center",
-   width : "auto",
+  chatHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "auto",
   },
-  header : {
-    backgroundColor : Colors.$backgroundDark,
-    },
-  container : {
+  header: {
+    backgroundColor: Colors.$backgroundDark,
+  },
+  container: {
 
-    width : "100%",
-    },
+    width: "100%",
+  },
 
 })
