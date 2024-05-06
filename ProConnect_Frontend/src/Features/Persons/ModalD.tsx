bca1143ce1f5f0cd0640691179a30bd3b1a10287
hadiDanial <hadiDanial@users.noreply.github.com>
2024-05-06 17:49:41 +0300

    Fix Send Offer ModalD Design

--- ProConnect_Frontend/src/Features/Persons/ModalD.tsx
+++ ProConnect_Frontend/src/Features/Persons/ModalD.tsx
@@ -1,59 +1,68 @@
 import React, { useEffect, useState } from 'react'
-import { View ,StyleSheet,Text, TextInput, Dimensions, TouchableWithoutFeedback} from 'react-native'
-import RNPickerSelect from 'react-native-picker-select';
-import { RequestTypes } from '../../Constants/Values';
-import ProRNPickerSelect from '../../Components/Layout/ProRNPickerSelect';
-import ProTextInput from '../../Components/Controls/ProTextInput';
+import { StyleSheet, Text, TextInput } from 'react-native'
+import { View } from 'react-native-ui-lib'
+import { RequestTypes, defaultWidthValues } from '../../Constants/Values';
 import { Colors } from 'react-native-ui-lib';
 import ProButton from '../../Components/Controls/ProButton';
-import { set } from 'date-fns';
-import { ka } from 'date-fns/locale';
-import { PostJobOfferApiArg, User, usePostJobOfferMutation } from '../../Services/Redux/Api';
-import { auth } from '../../Services/Firebase/Firebase';
+import { JobOffer, usePostJobOfferMutation } from '../../Services/Redux/Api';
 import { useSelector } from 'react-redux';
 import { getSelectedJob1, getSelectedReceiverUser } from '../../Services/Redux/Slices/ChatSlice';
 import { getUserAccount } from '../../Services/Redux/Slices/AuthSlice';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { TouchableOpacity } from 'react-native';
 
 
 interface ModalDProps {
-  send : (message : string) => void;
-  setModalVisible : (visible : boolean) => void;
-  userName : string;
+  send: (message: string) => void;
+  setModalVisible: (visible: boolean) => void;
+  userName: string;
 }
 
 
-const ModalD :React.FC<ModalDProps> =  (props) => {
+const ModalD: React.FC<ModalDProps> = (props) => {
   const [displayText, setDisplayText] = useState('');
+  const [initialOfferText, setInitialOfferText] = useState('');
 
   let budget = 0;
   const [RequestType, setRequestType] = useState("Job Offer")
   const [Budget, setBudget] = useState("0")
   const [Index, setIndex] = useState(0)
-  const text = "This is a " + RequestType + "\n Budget :" + Budget
-  const words = text.split(' ');
 
   const ReceiverUser = useSelector(getSelectedReceiverUser);
   const job = useSelector(getSelectedJob1)
   const senderUser = useSelector(getUserAccount)
   const [postJobOffer] = usePostJobOfferMutation();
+  const width = defaultWidthValues();
 
   useEffect(() => {
-    let currentIndex = 0;
-    const interval = setInterval(() => {
-      if (currentIndex < words.length) {
-        setDisplayText((prevText) => prevText + ' ' + words[currentIndex]);
-        currentIndex++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 50);
+    let interval: any = undefined;
+    const timeOutId = setTimeout(() => {
+      let currentIndex = 0;
+      const text = "This is a job offer for the job:\n" + job?.title + "\n\nCost Estimate: " + (Budget || 0);
+      const words = text.split(' ');
+      setDisplayText('');
+      setInitialOfferText(text);
+      interval = setInterval(() => {
+        if (currentIndex < words.length) {
+          const currentWord = words[currentIndex];
+          if (currentWord !== undefined) {
+            setDisplayText((prevText) => prevText + ' ' + currentWord);
+            currentIndex++;
+          }
+        }
+      }, 50);
 
-    return () => clearInterval(interval);
-  }, [RequestType,Budget]);
+    }, 250);
+    return () => {
+      clearTimeout(timeOutId);
+      if (interval) clearInterval(interval);
+    }
+
+  }, [RequestType, Budget]);
 
 
 
-  function onChange(RequestType : string, Budget : string, index : number){
+  function onChange(RequestType: string, Budget: string, index: number) {
     setRequestType(RequestTypes[Index].label);
     setBudget(Budget);
     setDisplayText('');
@@ -61,62 +70,63 @@ const ModalD :React.FC<ModalDProps> =  (props) => {
   }
 
   return (
+    <View margin-20 center backgroundColor={Colors.white} style={[styles.container, { width: (typeof width === 'number' ? width + 50 : width) }]}>
+      <TouchableOpacity style={{ position: 'absolute', top: 35, right: 30 }} onPress={() => {
+        props.setModalVisible(false);
+      }} >
+        <Entypo name="cross" size={24} color={Colors.black} />
+        <Entypo name="circle" size={24} color={Colors.black} style={{ position: 'absolute', transform: [{ scale: 1.3 }] }} />
+      </TouchableOpacity>
 
-
-
-    
-    <View style={styles.container}>
-      <View style={{alignItems : "center", width : "90%"}}>
-      <Text style={{color : "black", fontSize : 20, fontWeight : "bold"}}>Send a Request</Text>
+      <View invisible style={{ alignItems: "center" }}>
+        <Text style={{ color: "black", fontSize: 20, fontWeight: "bold" }}>Send a Request</Text>
       </View>
 
-      <View style={{alignItems : "center", width : "100%", right : 20}}>
-
-      <Text style={{color : "green"}}>Increase your chances</Text>
+      <View invisible marginB-5 style={{ alignItems: "center" }}>
+        <Text style={{ color: "green", alignSelf: 'center' }}>Increase your chances</Text>
       </View>
-     
-      <View style={{width : "90%"}}>
-        <View style={styles.RequestTypeContainer}>
+
+      <View invisible style={styles.w95}>
+        {/* <View invisible style={styles.RequestTypeContainer}>
           <Text style={styles.textStyle}>Request Type:</Text>
-          
-          <ProRNPickerSelect  data={RequestTypes} onValueChange={(value) => { onChange(value, Budget, Number(value) - 1)} } index={Index} />
-        </View>
 
-        <View style={styles.ReceiverContainer}>
+          <ProRNPickerSelect data={RequestTypes} onValueChange={(value) => { onChange(value, Budget, Number(value) - 1) }} index={Index} />
+        </View> */}
 
+        <View invisible row marginT-10 style={{ flexWrap: "wrap" }}>
           <Text style={styles.textStyle}>To:  </Text>
-
           <Text style={styles.textStyle}>{props.userName}</Text>
         </View>
+        {job && <View invisible row marginV-10>
+          <Text style={styles.textStyle}>Job:  {job?.title}</Text>
+        </View>}
 
-        <View style={styles.BudgetContainerContainer}>
-          <Text style={styles.textStyle}>Budget     </Text>
+        <View invisible spread row style={styles.BudgetContainerContainer}>
+          <Text style={styles.textStyle}>Price</Text>
 
-          <TextInput placeholder='Set Budget' style={{backgroundColor : "silver" , width : "90%", height : 50, paddingLeft : 10} } onChangeText={(text)=>{onChange(RequestType,text,Index)}}/>
+          <TextInput inputMode='numeric' placeholder='Set Price' style={{ backgroundColor: "silver", height: 50, paddingLeft: 10, flexGrow: 1, marginLeft: 10 }} onChangeText={(text) => { setBudget(text) }} />
         </View>
 
-        <View style={styles.messageContainer}>
+        <View invisible style={styles.messageContainer}>
           <Text>Message</Text>
-          <TextInput multiline={true} style={styles.messageStyle} editable={false} value={displayText}/>
+          <TextInput multiline={true} style={styles.messageStyle} editable={false} value={displayText} />
         </View>
 
-        <View style={styles.buttonsContainer}>
-        <ProButton text={"Send"} mobileWidth={150} onPress={()=>{
-          const jobOffer = {jobOffer : {senderUser : senderUser, receiverUser : ReceiverUser, job : job, description : displayText, bid : Number(Budget)}}
-          console.log(jobOffer)
-          postJobOffer(jobOffer)
-          props.send(text);
-          props.setModalVisible(false);
-        }}/>
-        <ProButton text={"Close"} mobileWidth={150} onPress={()=>{
-        props.setModalVisible(false);
-        }}/>
+        <View invisible style={{ alignItems: "center" }} marginT-15>
+          <ProButton text={"Send"} onPress={() => {
+            if (!senderUser || !ReceiverUser || !job) { console.log("Error: Missing data!"); return; }
+            const jobOffer: JobOffer = { senderUser: senderUser, receiverUser: ReceiverUser, job: job, description: displayText, bid: Number(Budget) }
+            console.log(jobOffer)
+            postJobOffer({ jobOffer: jobOffer })
+            props.send(initialOfferText);
+            props.setModalVisible(false);
+          }} />
+          {/* <ProButton text={"Close"} mobileWidth={150} onPress={() => {
+            props.setModalVisible(false);
+          }} /> */}
         </View>
       </View>
     </View>
-
-      
-    
   )
 }
 
@@ -124,16 +134,12 @@ export default ModalD
 
 
 const styles = StyleSheet.create({
-
-  container : {
-    marginTop : 100,
-    marginLeft : 20,
-    backgroundColor: "white",
+  container: {
     borderRadius: 20,
-    paddingLeft :35 ,
-    paddingTop : 35,
-    alignItems: 'flex-start',
-    justifyContent : "space-between",
+    alignSelf: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: "space-between",
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -142,60 +148,58 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+    padding: 35
     // Change the width here
-    width: 375, // Adjust this value as needed
-    height: 700, // Adjust this value as needed
   },
 
   RequestTypeContainer: {
     flexDirection: "row",
-    alignItems : "center",
-    height : 68,
-    justifyContent : "space-between",
-    marginBottom : 20,
+    alignItems: "center",
+    height: 68,
+    justifyContent: "space-between",
+    marginBottom: 20,
   },
 
-  ReceiverContainer : {
+  ReceiverContainer: {
     flexDirection: "row",
-    alignItems : "center",
-    height : 68,
-    width : "50%",
-    marginBottom : 20,
+    alignItems: "center",
+    // height: 68,
+    width: "50%",
+    marginBottom: 10,
   },
-  BudgetContainerContainer : {
+  BudgetContainerContainer: {
+    // flexDirection: "row",
+    alignItems: "center",
+    height: 68,
+    marginBottom: 20,
+
+  },
+  textStyle: {
+    color: "black",
+    fontSize: 15,
+  },
+
+  buttonsContainer: {
     flexDirection: "row",
-    alignItems : "center",
-    height : 68,
-    width : "90%",
-    marginBottom : 20,
+    justifyContent: "space-between",
+    width: "100%",
+    height: 100,
 
   },
-  textStyle : {
-    color : "black",
-    fontSize : 15,
-    },
-
-  buttonsContainer : {
-    flexDirection: "row",
-    justifyContent : "space-between",
-    width : "100%",
-    height : 100,
-
+  messageStyle: {
+    height: 200,
+    backgroundColor: "silver",
+    justifyContent: "flex-start",
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 10,
+    textAlignVertical: "top",
+    textAlign: "left",
+    color: "black",
   },
-  messageStyle:{
-    height : 200,
-    backgroundColor : "silver",
-    justifyContent : "flex-start",
-    paddingLeft : 8,
-    paddingRight : 8,
-    paddingTop : 10,
-    textAlignVertical : "top",
-    textAlign : "left",
-    color : "black",
-  },
-  messageContainer : {
-    justifyContent : "space-between",
-    height : 222,
+  messageContainer: {
+    justifyContent: "space-between",
+    height: 222,
   },
 
 });
